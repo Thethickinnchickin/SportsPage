1171b1514245ba935ea35c5ace11de430d3eb109
Thethickinnchickin <matt.reiley@sjsu.edu>
2022-06-26 15:48:20 -0700

    first commit:

---
+++ app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='row'>
+          <div className='col'>
+          <iframe src="http://weakstreams.com/streams/9862462"></iframe>
+          </div>
+          <div className='col'>
+          <iframe src="http://weakstreams.com/streams/9862462"></iframe>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+          <iframe src="http://weakstreams.com/streams/9862462"></iframe>
+          </div>
+          <div className='col'>
+          <iframe src="http://weakstreams.com/streams/9862462"></iframe>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
