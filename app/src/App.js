e57b913c0bcb881d7ee389e35d0bbea8c875f520
Matt <matt.reiley@sjsu.edu>
2022-08-14 12:24:23 -0700

    Commit

--- app/src/App.js
+++ app/src/App.js
@@ -2,34 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-   const athletics = 'https://embedstream.me/los-angeles-angels-vs-oakland-athletics-stream-2'
-   const padres = 'https://embedstream.me/san-diego-padres-vs-colorado-rockies-stream-2'
-   const giants = 'https://embedstream.me/san-francisco-giants-vs-los-angeles-dodgers-stream-3'
-   const soccer = 'https://embedstream.me/seattle-sounders-vs-fc-dallas-stream-1'
+   const braves_marlins = 'https://embedstream.me/miami-marlins-vs-atlanta-braves-stream-1'
+   const guardians_bluejays = 'https://embedstream.me/toronto-blue-jays-vs-cleveland-guardians-stream-1'
+   const orioles_rays = 'https://embedstream.me/tampa-bay-rays-vs-baltimore-orioles-stream-1'
+   const cubs_reds = 'https://embedstream.me/cincinnati-reds-vs-chicago-cubs-stream-1'
+   const padres_nationals = 'https://embedstream.me/washington-nationals-vs-san-diego-padres-stream-2'
+   const tigers_whitesox = 'https://embedstream.me/chicago-white-sox-vs-detroit-tigers-stream-1'
+   const philies_mets = 'https://embedstream.me/new-york-mets-vs-philadelphia-phillies-stream-1'
+   const dodgers_royals = 'https://embedstream.me/kansas-city-royals-vs-los-angeles-dodgers-stream-1'
+   const athletics_astros = 'https://embedstream.me/houston-astros-vs-oakland-athletics-stream-1'
+   const mariners_rangers = 'https://embedstream.me/texas-rangers-vs-seattle-mariners-stream-1'
+   const yankees_redsox = 'https://embedstream.me/boston-red-sox-vs-new-york-yankees-stream-1'
+   const brewers_cardinals = 'https://embedstream.me/st-louis-cardinals-vs-milwaukee-brewers-stream-1'
+   const diamondbacks_rockies = 'https://embedstream.me/colorado-rockies-vs-arizona-diamondbacks-stream-1'
+   const pirates_giants = 'https://embedstream.me/san-francisco-giants-vs-pittsburgh-pirates-stream-1'
+   const twins_angels = 'https://embedstream.me/los-angeles-angels-vs-minnesota-twins-stream-1'
+   const nascar = 'https://embedstream.me/nascar-federated-auto-parts-400-cup-series-stream-1'
+   const soccer1 = 'https://embedstream.me/los-angeles-galaxy-vs-vancouver-whitecaps-stream-1'
+   const soccer2 = 'https://embedstream.me/los-angeles-fc-vs-charlotte-stream-2'
   return (
     <div className="App">
         <div id="NW">
 
-        <iframe src={athletics} width='100%' height='100%' scrolling='yes'
+        <iframe src={athletics_astros} width='100%' height='100%' scrolling='yes'
          frameborder='0' allowfullscreen allowtransparency 
          referrerpolicy='unsafe-url'></iframe>
         </div>
         
         <div id="NE">
-        <iframe src={padres} width='100%' height='100%' scrolling='yes'
+        <iframe src={orioles_rays} width='100%' height='100%' scrolling='yes'
          frameborder='0' allowfullscreen allowtransparency
           referrerpolicy='unsafe-url'></iframe>
         </div>
 
         <div id="SE">
         
-        <iframe src={giants} width='100%' height='100%'
+        <iframe src={brewers_cardinals} width='100%' height='100%'
          scrolling='yes' frameborder='0' allowfullscreen allowtransparency
           referrerpolicy='unsafe-url'></iframe>
         </div>
 
         <div id="SW">
-        <iframe src={soccer} width='100%' height='100%' scrolling='yes'
+        <iframe src={nascar} width='100%' height='100%' scrolling='yes'
          frameborder='0' allowfullscreen allowtransparency
           referrerpolicy='unsafe-url'></iframe>
         </div>
