d835b23ac103106f9992356136290fb0d5b644bf
Matt <matt.reiley@sjsu.edu>
2022-08-19 16:15:53 -0700

    ssfdfds

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-   const reds_phillies = 'https://embedstream.me/cincinnati-reds-vs-philadelphia-phillies-stream-1'
-   const marlins_padres = 'https://embedstream.me/miami-marlins-vs-san-diego-padres-stream-1'
-   const yankess_rays = 'https://embedstream.me/new-york-yankees-vs-tampa-bay-rays-stream-1'
-   const pirates_redsox = 'https://embedstream.me/pittsburgh-pirates-vs-boston-red-sox-stream-1'
-   const nationals_cubs = 'https://embedstream.me/washington-nationals-vs-chicago-cubs-stream-1'
-   const bluejays_orioles = 'https://embedstream.me/toronto-blue-jays-vs-baltimore-orioles-stream-1'
-   const guardians_tigers = 'https://embedstream.me/cleveland-guardians-vs-detroit-tigers-stream-1'
-   const braves_mets = 'https://embedstream.me/atlanta-braves-vs-new-york-mets-stream-1'
-   const twins_royals = 'https://embedstream.me/minnesota-twins-vs-kansas-city-royals-stream-1'
-   const cardinals_rockies = 'https://embedstream.me/st-louis-cardinals-vs-colorado-rockies-stream-1'
-   const rangers_athletics = 'https://embedstream.me/texas-rangers-vs-oakland-athletics-stream-1'
-   const whitesox_astros = 'https://embedstream.me/chicago-white-sox-vs-houston-astros-stream-1'
-   const brewers_dodgers = 'https://embedstream.me/milwaukee-brewers-vs-los-angeles-dodgers-stream-1'
-   const angels_mariners = 'https://embedstream.me/los-angeles-angels-vs-seattle-mariners-stream-1'
-   const giants_diamondbacks = 'https://embedstream.me/san-francisco-giants-vs-arizona-diamondbacks-stream-1'
-   const soccer = 'https://embedstream.me/los-angeles-fc-vs-dc-united-stream-1'
+
+
+   /* BASEBALL */
+
+   const brewers_cubs = 'https://embedstream.me/chicago-cubs-vs-milwaukee-brewers-stream-1'
+   const redsox_orioles = 'https://embedstream.me/baltimore-orioles-vs-boston-red-sox-stream-1'
+   const mets_phillies = 'https://embedstream.me/philadelphia-phillies-vs-new-york-mets-stream-1'
+   const red_pirates = 'https://embedstream.me/pittsburgh-pirates-vs-cincinnati-reds-stream-1'
+   const bluejays_yankees = 'https://embedstream.me/new-york-yankees-vs-toronto-blue-jays-stream-1'
+   const royals_rays = 'https://embedstream.me/tampa-bay-rays-vs-kansas-city-royals-stream-1'
+   const angels_tigers = 'https://embedstream.me/detroit-tigers-vs-los-angeles-angels-stream-1'
+   const whitesox_guardians = 'https://embedstream.me/cleveland-guardians-vs-chicago-white-sox-stream-1'
+   const astros_braves = 'https://embedstream.me/atlanta-braves-vs-houston-astros-stream-1'
+   const rangers_twins = 'https://embedstream.me/minnesota-twins-vs-texas-rangers-stream-1'
+   const giants_rockies = 'https://embedstream.me/colorado-rockies-vs-san-francisco-giants-stream-1'
+   const mariners_athletics = 'https://embedstream.me/oakland-athletics-vs-seattle-mariners-stream-1'
+   const cardinals_diamondbacks = 'https://embedstream.me/arizona-diamondbacks-vs-st-louis-cardinals-stream-1'
+   const nationals_padres = 'https://embedstream.me/san-diego-padres-vs-washington-nationals-stream-1'
+   const marlines_dodgers = 'https://embedstream.me/los-angeles-dodgers-vs-miami-marlins-stream-1'
+
+   /* FOOTBALL */
+
+   const panthers_patriots = 'https://embedstream.me/new-england-patriots-vs-carolina-panthers-stream-1'
+   const saints_packers = 'https://embedstream.me/green-bay-packers-vs-new-orleans-saints-stream-1'
+   const texans_rams = 'https://embedstream.me/los-angeles-rams-vs-houston-texans-stream-1'
+
  
   return (
     <div className="App">
