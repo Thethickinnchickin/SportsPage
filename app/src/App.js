a497f1e2a3d3a230c4ec44d80d9d7ee7fe8a865e
Matt <matt.reiley@sjsu.edu>
2023-06-28 23:25:54 -0700

    c

--- app/src/App.js
+++ app/src/App.js
@@ -27,7 +27,7 @@ export function App() {
 
     /* NBA */
 
-    const warriors = 'https://embedsream/go-statrranto-kings-stream-2'
+    const warriors = 'https://embedsream/gorranto-kings-stream-2'
     const nets = 'https://embestrnda-trem-2'
 
 
