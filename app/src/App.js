ff02da7dee2627ee23eea32f01f4760ffc2a10c5
Matt <matt.reiley@sjsu.edu>
2023-07-14 00:03:08 -0700

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ export function App() {
 
     const falcons = 'https://embedstream.me/atlanta-falcons-vs-pittsburgh-steelers-stream-1'
 
-    const rams = 'https://embedstream.me/los-gelesams-vs-seattle-seahawks-stream-1'
+    const rams = 'https://embedstream.me/los-gelesams-vsttle-seahawks-stream-1'
 
     /* HOCKEY */
     const sharks = 'https://embedsteam.me/los-angeles-clippers-vs-phix-suns-stream-1'
